28436c1091be6d9f84158183868402ca708653fd
Thomas Heller <th.heller@gmail.com>
2019-02-21 21:26:17 +0100

    init commit
    ported from fulcro-expo example, same setup.

--- src/main/test/app.cljs
+++ src/main/test/app.cljs
@@ -2,9 +2,8 @@
   (:require
     ["expo" :as ex]
     ["react-native" :as rn]
-    ["react" :as r]
-    [fulcro.client :as fc]
-    [fulcro.client.primitives :as fp :refer (defsc)]
+    ["react" :as react]
+    [reagent.core :as r]
     [shadow.expo :as expo]
     ))
 
@@ -13,10 +12,6 @@
 ;; must use path relative to :output-dir
 
 (defonce splash-img (js/require "../assets/shadow-cljs.png"))
-
-(defonce root-ref (atom nil))
-
-(defonce app-ref (atom nil))
 
 (def styles
   ^js (-> {:container
@@ -31,34 +26,16 @@
           (clj->js)
           (rn/StyleSheet.create)))
 
-(defsc Root [this props]
-  {:initial-state
-   (fn [p]
-     {::foo "hello world"})
-
-   :query
-   [::foo]}
-
-  (r/createElement rn/View #js {:style (.-container styles)}
-    (r/createElement rn/Text #js {:style (.-title styles)} "Hello!")
-    (r/createElement rn/Image #js {:source splash-img :style #js {:width 200 :height 200}})
-    (r/createElement rn/Text nil (pr-str props))))
+(defn root []
+  [:> rn/View {:style (.-container styles)}
+   [:> rn/Text {:style (.-title styles)} "Hello!"]
+   [:> rn/Image {:source splash-img :style {:width 200 :height 200}}]])
 
 (defn start
   {:dev/after-load true}
   []
-  (reset! app-ref (fc/mount @app-ref Root :i-got-no-dom-node)))
+  (expo/render-root (r/as-element [root])))
 
 (defn init []
-  (let [app
-        (fc/make-fulcro-client
-          {:client-did-mount
-           (fn [{:keys [reconciler] :as app}])
-
-           :reconciler-options
-           {:root-render expo/render-root
-            :root-unmount (fn [node])}})]
-
-    (reset! app-ref app)
-    (start)))
+  (start))
 
