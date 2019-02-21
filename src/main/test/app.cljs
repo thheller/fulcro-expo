b83716afe879aacf6192d0125322e12e2ecbb995
Thomas Heller <th.heller@gmail.com>
2019-02-21 12:21:37 +0100

    support release builds and asset requires

--- src/main/test/app.cljs
+++ src/main/test/app.cljs
@@ -8,9 +8,28 @@
     [shadow.expo :as expo]
     ))
 
+;; must use defonce and must refresh full app so metro can fill these in
+;; at live-reload time `require` does not exist and will cause errors
+;; must use path relative to :output-dir
+
+(defonce splash-img (js/require "../assets/shadow-cljs.png"))
+
 (defonce root-ref (atom nil))
 
 (defonce app-ref (atom nil))
+
+(def styles
+  ^js (-> {:container
+           {:flex 1
+            :backgroundColor "#fff"
+            :alignItems "center"
+            :justifyContent "center"}
+           :title
+           {:fontWeight "bold"
+            :fontSize 24
+            :color "blue"}}
+          (clj->js)
+          (rn/StyleSheet.create)))
 
 (defsc Root [this props]
   {:initial-state
@@ -20,8 +39,10 @@
    :query
    [::foo]}
 
-  (r/createElement rn/View nil
-    (r/createElement rn/Text nil "Hello from Fulcro!" (pr-str props))))
+  (r/createElement rn/View #js {:style (.-container styles)}
+    (r/createElement rn/Text #js {:style (.-title styles)} "Hello!")
+    (r/createElement rn/Image #js {:source splash-img :style #js {:width 200 :height 200}})
+    (r/createElement rn/Text nil (pr-str props))))
 
 (defn start
   {:dev/after-load true}
